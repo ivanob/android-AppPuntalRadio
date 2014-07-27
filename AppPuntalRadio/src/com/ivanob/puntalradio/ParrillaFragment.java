70a05c3b207909bba874b241d283346c6ec3b28f
Ivan Obeso <ivan.obeso1@gmail.com>
2014-07-27 19:29:07 +0200

    First version of the timetable fragment

--- AppPuntalRadio/src/com/ivanob/puntalradio/ParrillaFragment.java
+++ AppPuntalRadio/src/com/ivanob/puntalradio/ParrillaFragment.java
@@ -1,18 +1,29 @@
 package com.ivanob.puntalradio;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
 
 public class ParrillaFragment extends Fragment{
+	private WebView webViewTimetable;
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.parrilla_fragment, container, false);
+	}
+	
+	public void onActivityCreated(Bundle savedInstanceState) {
+		webViewTimetable= (WebView)getActivity().findViewById(R.id.webViewTimetable);
+		webViewTimetable.loadUrl("file:///android_asset/time_table.html");
+		WebSettings webSettings = webViewTimetable.getSettings();
+		webSettings.setTextSize(WebSettings.TextSize.SMALLEST);
+		super.onActivityCreated(savedInstanceState);
 	}
 }
